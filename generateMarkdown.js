e26103aaff7a05f9efd425206c6e3d1275d8258e
mkdkck <colin173@gmail.com>
2023-11-12 00:28:15 +1030

    finalized all the functions that required

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,27 +1,56 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+// a function to transform the license choosen to the badge with a link of further description of the license.
 function renderLicenseBadge(license) {
-
-    [![License](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
-    [![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
-    [![License](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
-    [![License](https://img.shields.io/badge/License-BSD_3--Clause-orange.svg)](https://opensource.org/licenses/BSD-3-Clause)   
+  switch (license) {
+    case 'MIT':
+      licenseBadge = '[![License](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+      break;
+    case 'Apache-2.0' :
+      licenseBadge = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+      break;
+    case 'GPL-V3' :
+      licenseBadge = '[![License](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
+      break;
+    case 'BSD-3-Clause' :
+      licenseBadge = '[![License](https://img.shields.io/badge/License-BSD_3--Clause-orange.svg)](https://opensource.org/licenses/BSD-3-Clause)'
+      break;
+    case 'None' :
+      licenseBadge = ''
+      break;
+  }
+  return licenseBadge;
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// a function to transform the license choosen to a link that give further information about the license.
+function renderLicenseLink(license) {
+  switch (license) {
+    case 'MIT':
+      licenseLink = 'https://opensource.org/licenses/MIT'
+      break;
+    case 'Apache-2.0' :
+      licenseLink = 'https://opensource.org/licenses/Apache-2.0'
+      break;
+    case 'GPL-V3' :
+      licenseLink = 'https://www.gnu.org/licenses/gpl-3.0'
+      break;
+    case 'BSD-3-Clause' :
+      licenseLink = 'https://opensource.org/licenses/BSD-3-Clause'
+      break;
+    case 'None' :
+      licenseLink = ''
+      break;
+  }
+  return licenseLink;
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(title,description,installation,usage,contributing,test,license,github,contact) {
+// a function to generate a professional Readme markdown with the variables typed in.
+function generateMarkdown({title,description,installation,usage,contribution,test,license,github,contact}) {
   return `# ${title}
 
 ## Description
+${renderLicenseBadge(license)}
+
 ${description}
+
 
 ## Table of Contents
 
@@ -31,30 +60,44 @@ ${description}
 - [License](#license)
 - [Contribute](#Contribute)
 - [Tests](#Tests)
-- [Information](#Information)
+- [Questions](#Questions)
 
 ## Installation
+Here is a detail instruction of how to install this application:
+
 ${installation}
 
 ## Usage
+This application can be used in the following scenario(s):
+
 ${usage}
 
 ## License
-${license}
+The application is covered under license: ${license}. 
 
-## Contribute
-${contributing}
+Here is more information about this license:
+
+${renderLicenseLink(license)}
+
+## Contribution
+You can contribute to this application by: 
+
+${contribution}
 
 ## Tests
+Here is a way to test this application:
+
 ${test}
 
-## Information
-### Creater Github
-${github}
+## Questions
+If you want to know more about this application, you can view the owner Github Profile via:
 
-### Contact Me
-${contact}
-`;
+  https://github.com/${github}
+
+for further more questions, you can also contact the owner via the following contact detail(s):
+
+  ${contact}
+`
 }
 
 module.exports = generateMarkdown;
